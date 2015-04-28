d629c5ab6da9eb836363e9b7ff1762e910793e6a
unknown <z003aknc@MD1A1E4C.ww004.siemens.net>
2015-04-28 20:23:48 +0200

    Finalizes commentation
    Finished commentation of calculator class.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,26 @@
 package main;
 
 import myCalculator.Calculator;
+import myCalculator.InvalidMathException;
+import myCalculator.ResultOutOfRangeException;
 
 public class Main {
 
 	public static void main(String[] args)
 	{
-		System.out.println(Calculator.exponentiate(12, 2));
-		System.out.println(Calculator.exponentiate(-12, 3));
-		System.out.println(Calculator.exponentiate(12, 0));
-		System.out.println(Calculator.exponentiate(0, 2));
-		System.out.println(Calculator.exponentiate(0, 1));
-		System.out.println(Calculator.exponentiate(0, 0));
+	    try
+	    {
+	        System.out.println(Calculator.exponentiate(12, 2));
+	        System.out.println(Calculator.exponentiate(-12, 3));
+	        System.out.println(Calculator.exponentiate(12, 0));
+	        System.out.println(Calculator.exponentiate(0, 2));
+	        System.out.println(Calculator.exponentiate(0, 1));
+	        System.out.println(Calculator.exponentiate(12, 768237857));   
+	    }
+	    catch(ResultOutOfRangeException exc)
+	    {
+	        System.out.println(exc.getMessage());
+	    }
 	}
 	
 }
--- src/myCalculator/Calculator.java
+++ src/myCalculator/Calculator.java
@@ -10,39 +10,43 @@ package myCalculator;
 public class Calculator {
 
 	/**
+	 * Calculates the addition of two values.
 	 * 
 	 * @param firstSummand:	 the first summand of the addition (number before the operation symbol)
 	 * @param secondSummand: the second summand of the addition (number after the operation symbol)
 	 * @return: 			 the result of adding the second summand to the first one
 	 */
-	public static double add(double firstSummand, double secondSummand)
+	public static double add(double firstSummand, double secondSummand) throws ResultOutOfRangeException
 	{		
 		return firstSummand + secondSummand;
 	}
 	
 	/**
+	 * Calculates the subtraction of two values.
 	 * 
 	 * @param minuend:		the minuend of the subtraction (number before the operation symbol)
 	 * @param subtrahend:	the subtrahend of the subtraction (number after the operation symbol)
 	 * @return:				the result of subtracting the subtrahend from the minuend
 	 */
-	public static double subtract(double minuend, double subtrahend)
+	public static double subtract(double minuend, double subtrahend) throws ResultOutOfRangeException
 	{
 		return subtrahend - minuend;
 	}
 	
 	/**
+	 * Calculates the multiplication of two values.
 	 * 
 	 * @param firstFactor:	the first factor of the multiplication (number before the operation symbol)
 	 * @param secondFactor:	the second factor of the mulitplication (number after the opeartion symbol)
 	 * @return:				the result of multiplying the first factor by the second one
 	 */
-	public static double multiply(double firstFactor, double secondFactor)
+	public static double multiply(double firstFactor, double secondFactor) throws ResultOutOfRangeException
 	{
 		return firstFactor * secondFactor;
 	}
 	
 	/**
+	 * Calculates the division of two values.
 	 * 
 	 * @param dividend:		the dividend of the division (number before the operation symbol)
 	 * @param divisor:		the divisor of the division (number after the operation symbol)
@@ -53,7 +57,14 @@ public class Calculator {
 		return dividend / divisor;
 	}
 	
-	public static double exponentiate(double base, int exponent)
+	/**
+	 * Calculates the exponentiation of a value by another one.
+	 * 
+	 * @param base:        the base of the exponentiation (lower number)
+	 * @param exponent:    the exponent of the exponentiation (number above the base)
+	 * @return:            the exponentiation of the base by an integer exponent
+	 */
+	public static double exponentiate(double base, int exponent) throws ResultOutOfRangeException
 	{
 		double result = 1;
 		
